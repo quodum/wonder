cdb26e1b1b04b3474433410d02ecf52b732684e8
Loredana <loredana.cirstea@gmail.com>
2018-12-16 21:45:30 +0100

    Remove ear lights, simplify commands

--- wonderwars/vader/vader_robot.py
+++ wonderwars/vader/vader_robot.py
@@ -6,11 +6,12 @@ from wonderwars.robot import RobotConnection
 class VaderRobot(RobotConnection):
     def appearance(self, robot):
         robot.commands.RGB.stage_front(0, 0, 1)
+        robot.commands.RGB.stage_ear_left(0, 0, 0)
+        robot.commands.RGB.stage_ear_right(0, 0, 0)
         robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
 
     def presents(self, robot):
-        robot.commands.RGB.stage_front(0, 0, 1)
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
+        self.appearance(robot);
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_08)
 
         # 4 ture
@@ -18,16 +19,14 @@ class VaderRobot(RobotConnection):
             robot.commands.body.stage_wheel_speeds  (-7.8, 7.8)
 
     def wins(self, robot):
-        robot.commands.RGB.stage_front(0, 0, 1)
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
+        self.appearance(robot);
 
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_07)
         robot.commands.body.stage_wheel_speeds  (-25.56, 25.56)
         robot.commands.body.stage_wheel_speeds  (25, -25)
 
     def loses(self, robot):
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
-        robot.commands.RGB.stage_front(0, 0, 255)
+        self.appearance(robot);
 
         robot.commands.head.stage_tilt_angle(-15)
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_05)
--- wonderwars/yoda/yoda_robot.py
+++ wonderwars/yoda/yoda_robot.py
@@ -5,16 +5,17 @@ from wonderwars.robot import RobotConnection
 
 class YodaRobot(RobotConnection):
     def appearance(self, robot):
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
         robot.commands.RGB.stage_front(0, 0, 0)
+        robot.commands.RGB.stage_ear_left(0, 0, 0)
+        robot.commands.RGB.stage_ear_right(0, 0, 0)
+        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
 
         for i in range(1,4):
             robot.commands.head.stage_tilt_angle(-15)
             robot.commands.head.stage_tilt_angle(15)
 
     def presents(self, robot):
-        robot.commands.RGB.stage_front(0, 0, 0)
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
+        self.appearance(robot);
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_05)
 
         # 4 ture
@@ -22,8 +23,7 @@ class YodaRobot(RobotConnection):
             robot.commands.body.stage_wheel_speeds (-7.8, 7.8)
 
     def wins(self, robot):
-        robot.commands.RGB.stage_front(0, 0, 0)
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
+        self.appearance(robot);
 
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_05)
         robot.commands.body.stage_wheel_speeds  (-35, 35)
@@ -31,15 +31,13 @@ class YodaRobot(RobotConnection):
         robot.commands.body.stage_wheel_speeds  (35, -35)
 
     def loses(self, robot):
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
-        robot.commands.RGB.stage_front(0, 0, 0)
+        self.appearance(robot);
 
         robot.commands.head.stage_tilt_angle(-15)
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_07)
 
     def rock(self, robot):
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
-        robot.commands.RGB.stage_front(0, 0, 0)
+        self.appearance(robot);
 
         for i in range(1,4):
             robot.commands.head.stage_tilt_angle(-15)
@@ -48,8 +46,7 @@ class YodaRobot(RobotConnection):
         robot.cmds.media.stage_audio(WWMedia.WWSound.WWSoundDash.CUSTOM_02)
 
     def paper(self, robot):
-        robot.commands.eyering.stage_eyering((False, False, True, False, False, False, False, False, False, False, True, False), 0.4)
-        robot.commands.RGB.stage_front(0, 0, 0)
+        self.appearance(robot);
 
         for i in range(1,4):
             robot.commands.head.stage_tilt_angle(-15)
